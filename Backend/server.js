d0f9a278ce9842c043d7025a1e7bd1cc301feb45
Aadithya-J <jlaadithya@gmail.com>
2025-03-12 15:27:56 +0530

    modified server to use supabase

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
-const childRoutes = require("./routes/childRoutes"); // New routes
-const testRoutes = require("./routes/testRoutes"); // New routes
-const visualRoutes = require("./routes/visualRoute");
-
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI =
-  "mongodb+srv://shivkamp:niggesh@cluster0.gxq4s.mongodb.net/";
-// "mongodb://localhost:27017/schonell"
 
-// Connect to MongoDB
-mongoose.connect(MONGODB_URI);
-mongoose.connection.once("open", () => console.log("Connected to MongoDB"));
+import authRoutes from "./routes/authRoutes.js";
+
 
 // Enable CORS
 app.use(
@@ -31,10 +22,6 @@ app.use(express.json());
 
 // Use routes
 app.use("/", authRoutes);
-app.use("/", userRoutes);
-app.use("/", childRoutes); // Added child routes
-app.use("/", testRoutes); // Added test routes
-app.use("/", visualRoutes);
 
 // Start server
 app.listen(PORT, () => {
