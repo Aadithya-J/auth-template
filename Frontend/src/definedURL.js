4faf85c9609993c9bb72767ec87a8684bd0e1a80
Aadithya <jlaadithya@gmail.com>
2025-06-07 17:27:54 +0530

    Delete Frontend directory

--- Frontend/src/definedURL.js
+++
@@ -1,5 +0,0 @@
-// export const backendURL = "https://jiveeshadeployed.onrender.com/api";
-export const backendURL = "http://localhost:3000/api";
-
-//export const pythonURL = "https://jiveesha-python.onrender.com";
-//export const pythonURL = "http://localhost:5001";
