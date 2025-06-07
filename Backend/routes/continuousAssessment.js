78c4610c5367fa1edda85e306a8686426856feed
Aadithya <jlaadithya@gmail.com>
2025-06-07 17:27:37 +0530

    Delete Backend directory

--- Backend/routes/continuousAssessment.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { submitResults,getContinuousAssessmentResultsByChildId } from '../controllers/continuousAssessment.js';
-
-const router = Router();
-
-router.post('/continuous-assessment/submit', submitResults);
-router.get('/continuous-assessment/getByChildId/:child_id', getContinuousAssessmentResultsByChildId);
-export default router;
--- Backend/routes/ttsRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { ttsHandler } from "../controllers/ttsController.js";
-
-const router = express.Router();
-
-router.post("/tts", ttsHandler);
-
-export default router;
